d02c64f31273ab7dda0e252671527629423f7fda
Dwi <ruckuus@gmail.com>
2020-05-03 15:55:35 +0800

    Simple improvement to pass code revision in deployment.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ANSIBLE_SSH_ARGS="-C -o ControlMaster=auto -o ControlPersist=60s -o ControlPath=/tmp/ans-%h-%r" ansible -vvvv -i servers all -m ping
+ANSIBLE_SSH_ARGS="-C -o ControlMaster=auto -o ControlPersist=60s -o ControlPath=/tmp/ans-%h-%r" REVISION=`git log --pretty=format:"%H" -n 1` ansible-playbook -i servers deploy.yml -v
