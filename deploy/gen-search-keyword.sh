5479485435e103f6af76428ef59f7f06901f8caa
Dwi <ruckuus@gmail.com>
2020-05-05 11:43:43 +0800

    Generate top search keywords.

--- deploy/gen-search-keyword.sh
+++ deploy/gen-search-keyword.sh
@@ -2,12 +2,19 @@
 ROOT=http://dwisupriyadi20-nr-candidate-lab-app.eastus.cloudapp.azure.com:8080/shop/search/search.html
 
 declare -a KEYWORD=('java' 'node' 'js' 'javascript' 'programming' 'net' 'network' 'php' 'python' 'essential' 'algo')
+declare -a TOPKEYWORDS=('java' 'node' 'js' 'python')
 
 # Simulate search
 for r in "${KEYWORD[@]}"
 do
   curl -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0" -XPOST ${ROOT}?q=$r
-  sleep 5
+  sleep 2
+done
+
+for r in "${TOPKEYWORDS[@]}"
+do
+  curl -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0" -XPOST ${ROOT}?q=$r
+  sleep 2
 done
 
 
